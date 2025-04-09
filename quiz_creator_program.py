1768f2f523d0da7adc80ab3e1acfd616affb6901
Chloe <lunamarychloeanne@gmail.com>
2025-04-09 20:23:41 +0800

    If the user chose to input a question they need to enter the category

--- quiz_creator_program.py
+++ quiz_creator_program.py
@@ -7,3 +7,11 @@ while True:
     print("2. Exit the program")
 
     choice = input("\nEnter your choice (1 or 2): ")
+    
+    if choice == "1":
+        print("\nChoose a category for the quiz (Math, English, Science, Filipino)")
+        category = input("Enter the category: ").lower() # To lower every entered category
+
+        if category not in ["math", "english", "science", "filipino"]: 
+            print("\nInvalid category. Choose from Math, English, Science, or Filipino.")
+            continue
