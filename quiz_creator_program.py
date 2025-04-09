42a2fe53de8af2a01f895302eda4ab4487a0065e
Chloe <lunamarychloeanne@gmail.com>
2025-04-09 20:11:22 +0800

    Input the choice if you want to enter a question or exit the program

---
+++ quiz_creator_program.py
@@ -0,0 +1,9 @@
+print("---Quiz Question Creator---")
+ 
+# Choose between entering a question or exiting the program
+while True:
+    print("\nMenu:")
+    print("1. Enter a question")
+    print("2. Exit the program")
+
+    choice = input("\nEnter your choice (1 or 2): ")
